856f5a2970bf2f549e245cfd791f7f7880449b87
David Chapela de la Campa <david.chapela.delacampa@gmail.com>
2023-01-19 19:37:09 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='pix_utils',
+    version='0.1.0',
+    package_dir={"": "src"},
+    include_package_data=True,
+    install_requires=[
+        'numpy',
+        'scipy',
+        'setuptools'
+    ]
+)
