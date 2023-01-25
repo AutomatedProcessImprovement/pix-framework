d2f2ca714c51956a8b6fe5307eea4d97d36b01fe
David Chapela de la Campa <david.chapela.delacampa@gmail.com>
2023-01-25 19:43:34 +0200

    Add basic Event Log IDs

---
+++ src/pix_utils/__init__.py
@@ -0,0 +1,1 @@
+__all__ = ['log_ids', 'calendar', 'statistics']
---
+++ src/pix_utils/log_ids.py
@@ -0,0 +1,32 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class EventLogIDs:
+    case: str = 'case'  # Case ID
+    activity: str = 'activity'  # Activity label
+    enabled_time: str = 'enabled_time'  # Enablement time of the activity instance
+    start_time: str = 'start_timestamp'  # Start time of the activity instance
+    end_time: str = 'end_timestamp'  # End time of the activity instance
+    available_time: str = 'available_time'  # Last availability time of the resource who performed this activity instance
+    estimated_start_time: str = 'estimated_start_time'  # Estimated start time of the activity instance
+    resource: str = 'resource'  # Resource who performed this activity instance
+
+
+DEFAULT_CSV_IDS = EventLogIDs(case='case_id',
+                              activity='Activity',
+                              enabled_time='enabled_time',
+                              start_time='start_time',
+                              end_time='end_time',
+                              available_time='available_time',
+                              estimated_start_time='estimated_start_time',
+                              resource='Resource')
+
+DEFAULT_XES_IDS = EventLogIDs(case='case:concept:name',
+                              activity='concept:name',
+                              enabled_time='time:enabled',
+                              start_time='time:start',
+                              end_time='time:timestamp',
+                              available_time='time:available',
+                              estimated_start_time='time:estimated_start',
+                              resource='org:resource')
