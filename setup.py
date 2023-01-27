9ee156e411f3a1a261cb6f65c1af0a251f3553fc
David Chapela de la Campa <david.chapela.delacampa@gmail.com>
2023-01-27 12:35:01 +0200

    Implement method to obtain the non-working intervals within a specified period of time

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='pix_utils',
-    version='0.2.0',
+    version='0.3.0',
     package_dir={"": "src"},
     include_package_data=True,
     install_requires=[
