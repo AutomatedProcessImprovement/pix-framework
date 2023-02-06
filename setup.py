d2db620ae50d87641a28d46c085093bd5e79cde7
David Chapela de la Campa <david.chapela.delacampa@gmail.com>
2023-02-06 17:44:08 +0200

    Add functions to split an event log in two sets by events (possibility of partial traces in each set) or traces (full traces in each set)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='pix_utils',
-    version='0.3.0',
+    version='0.4.0',
     package_dir={"": "src"},
     include_package_data=True,
     install_requires=[
