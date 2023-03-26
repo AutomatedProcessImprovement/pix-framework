25c8a5fddb47c28712831afd6196845fa7f1bf1f
David Chapela de la Campa <david.chapela.delacampa@gmail.com>
2023-03-26 23:17:40 +0300

    Make interval timings timezone aware when comparing for last available computation

--- setup.py
+++ setup.py
@@ -2,13 +2,14 @@ from setuptools import setup
 
 setup(
     name='pix_utils',
-    version='0.6.1',
+    version='0.6.2',
     package_dir={"": "src"},
     include_package_data=True,
     install_requires=[
         'pandas',
         'numpy',
         'scipy',
-        'setuptools'
+        'setuptools',
+        'pytz'
     ]
 )
