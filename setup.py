52f2198e6a04abc0392cb4a3cd26a5c9f55027dd
David Chapela de la Campa <david.chapela.delacampa@gmail.com>
2023-04-04 16:38:14 +0300

    Fix typo in "enabling_activity" log id

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='pix_utils',
-    version='0.6.3',
+    version='0.6.4',
     package_dir={"": "src"},
     include_package_data=True,
     install_requires=[
