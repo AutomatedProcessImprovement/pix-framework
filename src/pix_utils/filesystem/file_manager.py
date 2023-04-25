3178d0c7266f29776dd7fce2592cf597c11453fc
David Chapela de la Campa <david.chapela.delacampa@gmail.com>
2023-04-25 16:41:21 +0300

    Add file manager methods

---
+++ src/pix_utils/filesystem/file_manager.py
@@ -0,0 +1,46 @@
+import datetime
+import os
+import shutil
+import uuid
+from pathlib import Path
+
+
+def get_project_dir() -> Path:
+    return Path(os.path.dirname(__file__)).parent.parent
+
+
+def get_random_folder_id(prefix: str = '') -> str:
+    return f'{prefix}{get_random_id()}'
+
+
+def get_random_file_id(extension: str, prefix: str = '') -> str:
+    return f'{prefix}{get_random_id()}{extension}'
+
+
+def get_random_id() -> str:
+    return f'{datetime.datetime.today().strftime("%Y%m%d_%H%M%S")}_{str(uuid.uuid4()).upper().replace("-", "_")}'
+
+
+def create_folder(path: Path) -> bool:
+    if os.path.exists(path):
+        return False
+    else:
+        os.makedirs(path)
+        return True
+
+
+def remove_asset(path: Path):
+    if path is not None and path.exists():
+        if path.is_dir():
+            shutil.rmtree(path)
+        elif path.is_file():
+            path.unlink()
+
+
+def create_new_tmp_folder(base_path: Path) -> Path:
+    # Get non existent folder name
+    output_folder = base_path.joinpath(get_random_folder_id())
+    while not create_folder(output_folder):
+        output_folder = base_path.joinpath(get_random_folder_id())
+    # Return path to new folder
+    return output_folder
