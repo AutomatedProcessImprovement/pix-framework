c798752bafed4c31feeb0eaf8432b4aba54ad5e6
David Chapela de la Campa <david.chapela.delacampa@gmail.com>
2023-05-04 15:04:49 +0300

    Add function to remove outliers from a list of values

---
+++ src/pix_utils/statistics/utils.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+
+def remove_outliers(data, m=5.):
+    """
+    Remove outliers from a list of values following the approach presented in https://stackoverflow.com/a/16562028.
+    :param data: list of values.
+    :param m: maximum ratio between the difference (value - median) and the median of these differences, to NOT be
+    considered an outlier. Decreasing the [m] ratio increases the number of detected outliers (observations closer
+    to the median are considered as outliers).
+    :return: the received list of values without the outliers.
+    """
+    # Compute distance of each value from the median
+    data = np.asarray(data)
+    d = np.abs(data - np.median(data))
+    # Compute the median of these distances
+    mdev = np.median(d)
+    # Compute the ratio between each distance and the median of distances
+    s = d / mdev if mdev else 0.0
+    # Keep values with a ratio lower than the specified threshold
+    return data[s < m]
