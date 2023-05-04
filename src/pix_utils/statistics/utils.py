100e55ed1a3d2749da864f372da94da6bfe7d04b
David Chapela de la Campa <david.chapela.delacampa@gmail.com>
2023-05-04 17:06:38 +0300

    Fix minor error with outlier detection when median deviation is 0.0

--- src/pix_utils/statistics/utils.py
+++ src/pix_utils/statistics/utils.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-def remove_outliers(data:list, m=5.) -> list:
+def remove_outliers(data: list, m=5.) -> list:
     """
     Remove outliers from a list of values following the approach presented in https://stackoverflow.com/a/16562028.
     :param data: list of values.
@@ -16,6 +16,6 @@ def remove_outliers(data:list, m=5.) -> list:
     # Compute the median of these distances
     mdev = np.median(d)
     # Compute the ratio between each distance and the median of distances
-    s = d / mdev if mdev else 0.0
+    s = d / (mdev if mdev else 1.0)
     # Keep values with a ratio lower than the specified threshold
     return data[s < m].tolist()
