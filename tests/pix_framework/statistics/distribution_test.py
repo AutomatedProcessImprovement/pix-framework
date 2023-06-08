d2e92eb8429e8292a798f2fb02bfe5fb5c25dd46
Iryna Halenok <iryna.halenok@gmail.com>
2023-06-08 18:32:43 +0300

    Add unit tests for implemented (de)serialization, fix norm and expon serialization

--- tests/pix_framework/statistics/distribution_test.py
+++ tests/pix_framework/statistics/distribution_test.py
@@ -1,13 +1,10 @@
 import random
 
 import numpy as np
+import pytest
 import scipy.stats as st
-
 from pix_framework.statistics.distribution import (
-    get_best_fitting_distribution,
-    DurationDistribution,
-    DistributionType,
-)
+    DistributionType, DurationDistribution, get_best_fitting_distribution)
 
 
 def test_infer_distribution_fixed():
@@ -118,3 +115,46 @@ def test_scale_distributions():
         assert scaled.std == distribution.std * alpha
         assert scaled.min == distribution.min * alpha
         assert scaled.max == distribution.max * alpha
+
+def get_prosimos_dict(distribution_name, distribution_params):
+    params = []
+    for param in distribution_params:
+        params.append({"value": param})
+    
+    return {
+        "distribution_name": distribution_name,
+        "distribution_params": params
+    }
+
+duration_dict = [
+    (get_prosimos_dict("fix", [10]), DurationDistribution(DistributionType.FIXED, mean=10.0)),
+    (get_prosimos_dict("expon", [10, 100, 500]), DurationDistribution(DistributionType.EXPONENTIAL, mean=10.0, minimum=100.0, maximum=500.0)),
+    (get_prosimos_dict("uniform", [100, 200]), DurationDistribution(DistributionType.UNIFORM, minimum=100.0, maximum=200.0)),
+    (get_prosimos_dict("norm", [10, 20, 100, 200]), DurationDistribution(DistributionType.NORMAL, mean=10.0, std=20.0, minimum=100.0, maximum=200.0)),
+    (get_prosimos_dict("lognorm", [10, 20, 100, 200]), DurationDistribution(DistributionType.LOG_NORMAL, mean=10.0, var=20.0, minimum=100.0, maximum=200.0)),
+    (get_prosimos_dict("gamma", [10, 20, 100, 200]), DurationDistribution(DistributionType.GAMMA, mean=10.0, var=20.0, minimum=100.0, maximum=200.0)),
+]
+
+@pytest.mark.parametrize(
+    "duration_dict, expected_distribution",
+    duration_dict
+)
+
+def test_deserialization(duration_dict: dict, expected_distribution: DurationDistribution):
+    # act: perform the deserialization
+    actual_result = DurationDistribution.from_dict(duration_dict)
+
+    # assert: check whether class fields are equal by comparing strings
+    assert str(actual_result) == str(expected_distribution)
+
+@pytest.mark.parametrize(
+    "expected_prosimos_dict, input_distribution",
+    duration_dict
+)
+
+def test_serialization(expected_prosimos_dict: dict, input_distribution: DurationDistribution):
+    # act: convert class instance to dictionary used as an input for Prosimos
+    actual_result = input_distribution.to_prosimos_distribution()
+
+    # assert: compare received dictionaries
+    assert actual_result == expected_prosimos_dict
