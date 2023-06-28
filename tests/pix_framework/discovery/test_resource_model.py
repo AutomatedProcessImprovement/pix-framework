24d6492833b3d679b2d74a0477706cf89f06e2f0
David Chapela de la Campa <david.chapela.delacampa@gmail.com>
2023-06-28 20:36:07 +0300

    Add tests for discovery of resource pools, profiles, activity performances and model (missing calendar)

---
+++ tests/pix_framework/discovery/test_resource_model.py
@@ -0,0 +1,174 @@
+from pathlib import Path
+
+import pytest
+
+from pix_framework.calendar.resource_calendar import RCalendar
+from pix_framework.discovery.resource_activity_performances import ActivityResourceDistribution
+from pix_framework.discovery.resource_calendars import CalendarDiscoveryParams, CalendarType
+from pix_framework.discovery.resource_model import discover_resource_model, ResourceModel
+from pix_framework.discovery.resource_profiles import ResourceProfile
+from pix_framework.input import read_csv_log
+from pix_framework.log_ids import APROMORE_LOG_IDS
+
+assets_dir = Path(__file__).parent.parent / "assets"
+
+
+@pytest.mark.integration
+@pytest.mark.parametrize("log_name", ["Resource_profiles_test.csv"])
+def test_discover_case_arrival_model_undifferentiated(log_name):
+    log_path = assets_dir / log_name
+    log_ids = APROMORE_LOG_IDS
+    # Read event log
+    log = read_csv_log(log_path, log_ids)
+
+    # Discover resource model with undifferentiated resources
+    result = discover_resource_model(
+        event_log=log,
+        log_ids=log_ids,
+        params=CalendarDiscoveryParams(discovery_type=CalendarType.UNDIFFERENTIATED)
+    )
+    # Assert
+    assert type(result) is ResourceModel
+    assert len(result.resource_profiles) == 1
+    assert type(result.resource_profiles[0]) is ResourceProfile
+    assert result.resource_profiles[0].name == "Undifferentiated_resource_profile"
+    assert len(result.resource_calendars) == 1
+    assert type(result.resource_calendars[0]) is RCalendar
+    assert result.resource_calendars[0].calendar_id == "Undifferentiated_calendar"
+    assert len(result.activity_resource_distributions) == 5
+    assert type(result.activity_resource_distributions[0]) is ActivityResourceDistribution
+    assert len(
+        [
+            distribution.resource_id
+            for activity in result.activity_resource_distributions
+            for distribution in activity.activity_resources_distributions
+        ]
+    ) == 15
+
+
+@pytest.mark.integration
+@pytest.mark.parametrize("log_name", ["Resource_profiles_test.csv"])
+def test_discover_case_arrival_model_24_7(log_name):
+    log_path = assets_dir / log_name
+    log_ids = APROMORE_LOG_IDS
+    # Read event log
+    log = read_csv_log(log_path, log_ids)
+
+    # Discover resource model with 24/7 resources
+    result = discover_resource_model(
+        event_log=log,
+        log_ids=log_ids,
+        params=CalendarDiscoveryParams(discovery_type=CalendarType.DEFAULT_24_7)
+    )
+    # Assert
+    assert type(result) is ResourceModel
+    assert len(result.resource_profiles) == 1
+    assert type(result.resource_profiles[0]) is ResourceProfile
+    assert result.resource_profiles[0].name == "Undifferentiated_resource_profile"
+    assert len(result.resource_calendars) == 1
+    assert type(result.resource_calendars[0]) is RCalendar
+    assert result.resource_calendars[0].calendar_id == "24_7_CALENDAR"
+    assert len(result.activity_resource_distributions) == 5
+    assert type(result.activity_resource_distributions[0]) is ActivityResourceDistribution
+    assert len(
+        [
+            distribution.resource_id
+            for activity in result.activity_resource_distributions
+            for distribution in activity.activity_resources_distributions
+        ]
+    ) == 15
+
+
+@pytest.mark.integration
+@pytest.mark.parametrize("log_name", ["Resource_profiles_test.csv"])
+def test_discover_case_arrival_model_9_5(log_name):
+    log_path = assets_dir / log_name
+    log_ids = APROMORE_LOG_IDS
+    # Read event log
+    log = read_csv_log(log_path, log_ids)
+
+    # Discover resource model with 9/5 resources
+    result = discover_resource_model(
+        event_log=log,
+        log_ids=log_ids,
+        params=CalendarDiscoveryParams(discovery_type=CalendarType.DEFAULT_9_5)
+    )
+    # Assert
+    assert type(result) is ResourceModel
+    assert len(result.resource_profiles) == 1
+    assert type(result.resource_profiles[0]) is ResourceProfile
+    assert result.resource_profiles[0].name == "Undifferentiated_resource_profile"
+    assert len(result.resource_calendars) == 1
+    assert type(result.resource_calendars[0]) is RCalendar
+    assert result.resource_calendars[0].calendar_id == "9_5_CALENDAR"
+    assert len(result.activity_resource_distributions) == 5
+    assert type(result.activity_resource_distributions[0]) is ActivityResourceDistribution
+    assert len(
+        [
+            distribution.resource_id
+            for activity in result.activity_resource_distributions
+            for distribution in activity.activity_resources_distributions
+        ]
+    ) == 15
+
+
+@pytest.mark.integration
+@pytest.mark.parametrize("log_name", ["Resource_profiles_test.csv"])
+def test_discover_case_arrival_model_differentiated(log_name):
+    log_path = assets_dir / log_name
+    log_ids = APROMORE_LOG_IDS
+    # Read event log
+    log = read_csv_log(log_path, log_ids)
+
+    # Discover resource model with differentiated resources
+    result = discover_resource_model(
+        event_log=log,
+        log_ids=log_ids,
+        params=CalendarDiscoveryParams(discovery_type=CalendarType.DIFFERENTIATED_BY_RESOURCE)
+    )
+    # Assert
+    assert type(result) is ResourceModel
+    assert len(result.resource_profiles) == 3
+    assert type(result.resource_profiles[0]) is ResourceProfile
+    assert len(result.resource_calendars) == 3
+    assert type(result.resource_calendars[0]) is RCalendar
+    assert len(result.activity_resource_distributions) == 5
+    assert type(result.activity_resource_distributions[0]) is ActivityResourceDistribution
+    assert len(
+        [
+            distribution.resource_id
+            for activity in result.activity_resource_distributions
+            for distribution in activity.activity_resources_distributions
+        ]
+    ) == 8
+
+
+@pytest.mark.integration
+@pytest.mark.parametrize("log_name", ["Resource_profiles_test.csv"])
+def test_discover_case_arrival_model_pool(log_name):
+    log_path = assets_dir / log_name
+    log_ids = APROMORE_LOG_IDS
+    # Read event log
+    log = read_csv_log(log_path, log_ids)
+
+    # Discover resource model with pooled resources
+    result = discover_resource_model(
+        event_log=log,
+        log_ids=log_ids,
+        params=CalendarDiscoveryParams(discovery_type=CalendarType.DIFFERENTIATED_BY_POOL)
+    )
+    # Assert
+    assert type(result) is ResourceModel
+    assert len(result.resource_profiles) == 2
+    assert type(result.resource_profiles[0]) is ResourceProfile
+    assert len(result.resource_calendars) == 2
+    assert type(result.resource_calendars[0]) is RCalendar
+    assert len(result.activity_resource_distributions) == 5
+    assert type(result.activity_resource_distributions[0]) is ActivityResourceDistribution
+    assert len(
+        [
+            distribution.resource_id
+            for activity in result.activity_resource_distributions
+            for distribution in activity.activity_resources_distributions
+        ]
+    ) == 8
--- tests/pix_framework/discovery/test_resource_profiles.py
+++ tests/pix_framework/discovery/test_resource_profiles.py
@@ -1,24 +1,24 @@
 from pathlib import Path
 
-from pix_framework.discovery.resource_profiles import discover_undifferentiated_resource_profile
+import pytest
+
+from pix_framework.discovery.resource_profiles import discover_undifferentiated_resource_profile, \
+    discover_differentiated_resource_profiles, discover_pool_resource_profiles
 from pix_framework.input import read_csv_log
-from pix_framework.log_ids import DEFAULT_CSV_IDS
+from pix_framework.log_ids import APROMORE_LOG_IDS
 
 assets_dir = Path(__file__).parent.parent / "assets"
 
 
-def test_resource_profiles_undifferentiated_keep_names():
-    log_path = assets_dir / 'LoanApp_sequential_9-5_diffres_timers.csv'
-
-    log_ids = DEFAULT_CSV_IDS
-
+@pytest.mark.integration
+@pytest.mark.parametrize('log_name', ['Resource_profiles_test.csv'])
+def test_resource_profiles_undifferentiated(log_name):
+    log_path = assets_dir / log_name
+    log_ids = APROMORE_LOG_IDS
     log = read_csv_log(log_path, log_ids)
 
     # Discover undifferentiated profile keeping log resource names
-    undifferentiated_profile = discover_undifferentiated_resource_profile(
-        event_log=log,
-        log_ids=log_ids
-    )
+    undifferentiated_profile = discover_undifferentiated_resource_profile(event_log=log, log_ids=log_ids)
     # Assert discovered profile name is expected
     assert undifferentiated_profile is not None
     assert undifferentiated_profile.name == 'Undifferentiated_resource_profile'
@@ -30,14 +30,6 @@ def test_resource_profiles_undifferentiated_keep_names():
     log_activities = sorted(log[log_ids.activity].unique())
     for resource in undifferentiated_profile.resources:
         assert sorted(resource.assigned_tasks) == log_activities
-
-
-def test_resource_profiles_undifferentiated_simplify():
-    log_path = assets_dir / 'LoanApp_sequential_9-5_diffres_timers.csv'
-
-    log_ids = DEFAULT_CSV_IDS
-
-    log = read_csv_log(log_path, log_ids)
 
     # Discover undifferentiated profile keeping log resource names
     undifferentiated_profile = discover_undifferentiated_resource_profile(
@@ -55,3 +47,65 @@ def test_resource_profiles_undifferentiated_simplify():
     # Assert the resource has all activities assigned
     log_activities = sorted(log[log_ids.activity].unique())
     assert sorted(undifferentiated_profile.resources[0].assigned_tasks) == log_activities
+
+
+@pytest.mark.integration
+@pytest.mark.parametrize('log_name', ['Resource_profiles_test.csv'])
+def test_resource_profiles_differentiated(log_name):
+    log_path = assets_dir / log_name
+    log_ids = APROMORE_LOG_IDS
+    log = read_csv_log(log_path, log_ids)
+    # Discover differentiated profiles
+    differentiated_profiles = discover_differentiated_resource_profiles(event_log=log, log_ids=log_ids)
+    # Assert discovered profile name is expected
+    assert differentiated_profiles is not None
+    # Assert the resources are the ones from the log
+    log_resources = list(log[log_ids.resource].unique())
+    assert len(differentiated_profiles) == len(log_resources)
+    for differentiated_profile in differentiated_profiles:
+        # One resource in this profile
+        assert len(differentiated_profile.resources) == 1
+        # Name is one of the existing resources
+        resource = differentiated_profile.resources[0]
+        assert resource.name in log_resources
+        # They have their activities assigned to them
+        log_activities = sorted(log[log[log_ids.resource] == resource.name][log_ids.activity].unique())
+        assert sorted(resource.assigned_tasks) == log_activities
+
+
+@pytest.mark.integration
+@pytest.mark.parametrize('log_name', ['Resource_profiles_test.csv'])
+def test_resource_profiles_pools(log_name):
+    log_path = assets_dir / log_name
+    log_ids = APROMORE_LOG_IDS
+    log = read_csv_log(log_path, log_ids)
+    # Discover differentiated profiles
+    pooled_profiles = discover_pool_resource_profiles(event_log=log, log_ids=log_ids)
+    # Assert discovered pools is two
+    assert pooled_profiles is not None
+    assert len(pooled_profiles) == 2
+    # Assert the resources are the ones from the log
+    log_resources = list(log[log_ids.resource].unique())
+    assert len(
+        [resource.name for pool_profile in pooled_profiles for resource in pool_profile.resources]
+    ) == len(log_resources)
+    # Analyze each pool individually
+    for pool_profile in pooled_profiles:
+        # Expecting two pools:
+        if len(pool_profile.resources) == 2:
+            # Jotaro and Jolyne
+            log_activities = sorted(
+                log[log[log_ids.resource].isin(["Jotaro-000001", "Jolyne-000001"])][log_ids.activity].unique()
+            )
+            for resource in pool_profile.resources:
+                assert resource.name in ["Jotaro-000001", "Jolyne-000001"]
+                assert sorted(resource.assigned_tasks) == log_activities
+        else:
+            # Pucci
+            assert len(pool_profile.resources) == 1
+            resource = pool_profile.resources[0]
+            assert resource.name == "Pucci-000001"
+            log_activities = sorted(
+                log[log[log_ids.resource] == "Pucci-000001"][log_ids.activity].unique()
+            )
+            assert sorted(resource.assigned_tasks) == log_activities
