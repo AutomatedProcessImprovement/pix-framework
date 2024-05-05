fbc7e10e59c8d09ed6a89b90e409b3e662e3f969
Simple Squirrel <simplesquirrellearn@gmail.com>
2024-05-05 19:43:01 +0300

    Modified experiments run file

--- src/pix_framework/discovery/attributes/run.py
+++ src/pix_framework/discovery/attributes/run.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from attributes import discover_attributes
+from attribute_discovery import discover_attributes
 from pix_framework.io.event_log import EventLogIDs
 import pprint
 import time
@@ -16,62 +16,35 @@ PROSIMOS_LOG_IDS = EventLogIDs(
 )
 
 # Constants for file paths
-CASE_ATTRIBUTES_FILE_PATH = "tests/pix_framework/assets/case_attributes_log.csv"
-EVENT_ATTRIBUTES_FILE_PATH = "tests/pix_framework/assets/event_attributes_log.csv"
-CASE_AND_EVENT_ATTRIBUTES_FILE_PATH = "tests/pix_framework/assets/case_and_event_attributes_log.csv"
-GLOBAL_ATTRIBUTES_FILE_PATH = "tests/pix_framework/assets/global_attributes_log.csv"
-EVENT_AND_GLOBAL_FILE_PATH = "tests/pix_framework/assets/event_and_global_attributes_log.csv"
-MATH_TEST_PATH = "tests/pix_framework/assets/math_log.csv"
-DISCRETE_TEST_PATH = "tests/pix_framework/assets/discrete_log.csv"
+LOG_PATH_EXAMPLE = "D:/path/file.csv"
 EXPERIMENTS_TEST_PATH = "D:/_est/PIX_discovery/Experiments/experiment_log_main.csv"
-MG_EXPERIMENTS_SHORT_TEST_PATH = "D:/_est/PIX_discovery/Experiments/mg_experiment_log_main.csv"
-ME_EXPERIMENTS_SHORT_TEST_PATH = "D:/_est/PIX_discovery/Experiments/me_experiment_log_main.csv"
-SG_EXPERIMENTS_SHORT_TEST_PATH = "D:/_est/PIX_discovery/Experiments/sg_experiment_log_main.csv"
-SE_EXPERIMENTS_SHORT_TEST_PATH = "D:/_est/PIX_discovery/Experiments/se_experiment_log_main.csv"
-C_EXPERIMENTS_SHORT_TEST_PATH = "D:/_est/PIX_discovery/Experiments/c_experiment_log_main.csv"
-
-def fetch_and_print_attributes(file_name, method, sizes, log_ids):
-    for size in sizes:
-        start_time = time.time()  # Start time
-
-        event_log = pd.read_csv(file_name)
-
-        if size > 0:  # Only apply the filtering if size is greater than 0
-            subset_cases = event_log.drop_duplicates(subset='case_id').head(size)
-            event_log = event_log[event_log['case_id'].isin(subset_cases['case_id'])]
-
-        # Fetching the results from method and printing them
-        results = method(event_log, log_ids=log_ids)
-
-        end_time = time.time()  # End time
-        print(f"\n{file_name.split('/')[-1]} with head size {size} execution time: {end_time - start_time:.2f} seconds\n\n\n\n")
 
 
-def discover_and_print_for_files(method, file_paths, sizes, log_ids):
+def fetch_and_print_attributes(file_name, method, log_ids):
+    start_time = time.time()  # Start time
+
+    event_log = pd.read_csv(file_name)
+
+    # Fetching the results from method and printing them
+    results = method(event_log, log_ids=log_ids)
+
+    end_time = time.time()  # End time
+    print(f"\n{file_name.split('/')[-1]} execution time: {end_time - start_time:.2f} seconds\n\n\n\n")
+
+
+def discover_and_print_for_files(method, file_paths, log_ids):
     for file_name in file_paths:
         print(f"\n\n\n\n\nDISCOVERING {file_name}")
-        fetch_and_print_attributes(file_name, method, sizes, log_ids)
+        fetch_and_print_attributes(file_name, method, log_ids)
 
 
 if __name__ == "__main__":
-    sizes_to_test = [0]
     files_to_discover = [
-        # CASE_ATTRIBUTES_FILE_PATH,
-        # EVENT_ATTRIBUTES_FILE_PATH,
-        # CASE_AND_EVENT_ATTRIBUTES_FILE_PATH,
-        # GLOBAL_ATTRIBUTES_FILE_PATH,
-        # EVENT_AND_GLOBAL_FILE_PATH,
-        # MATH_TEST_PATH,
-        # DISCRETE_TEST_PATH,
-        # EXPERIMENTS_TEST_PATH,
-        # MG_EXPERIMENTS_SHORT_TEST_PATH,
-        # ME_EXPERIMENTS_SHORT_TEST_PATH,
-        # SG_EXPERIMENTS_SHORT_TEST_PATH,
-        # SE_EXPERIMENTS_SHORT_TEST_PATH,
-        # C_EXPERIMENTS_SHORT_TEST_PATH
+        LOG_PATH_EXAMPLE,
+        EXPERIMENTS_TEST_PATH
     ]
 
-    discover_and_print_for_files(discover_attributes, files_to_discover, sizes_to_test, PROSIMOS_LOG_IDS)
+    discover_and_print_for_files(discover_attributes, files_to_discover, PROSIMOS_LOG_IDS)
 
 
 
